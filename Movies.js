b827b0b56f65f55aac22fa65fca389a294ca3ee5
GiselleEmilia <114619090+GiselleEmilia@users.noreply.github.com>
2023-03-22 23:33:15 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -3,10 +3,24 @@ var Schema = mongoose.Schema;
 
 mongoose.connect(process.env.DB);
 
-// Movie schema
 var MovieSchema = new Schema({
+    title: { type: String, required: true, index: { unique: true }},
+    releaseDate: { type: String, required: true },
+    genre: {
+    type: String, 
+    enum:["Action","Adventure","Comedy","Drama","Fantasy","Horror","Mystery","Thriller","Western","Science Fiction",] },
+    actors: [
+        { actorname:{ type: String, required: true }, characterName: { type: String, required: true}},
+       // { actorname:{ type: String, required: true }, characterName: { type: String, required: true}},
+       // { actorname:{ type: String, required: true }, characterName: { type: String, required: true}}
+        ],
 
 });
+
+MovieSchema.pre('save', function(next){
+
+    next();
+})
 
 // return the model
 module.exports = mongoose.model('Movie', MovieSchema);
